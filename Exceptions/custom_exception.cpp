6464b513ffcce190c362eca6d8f56beafbbb19b3
programmercave0 <mailabhi593@gmail.com>
2022-09-19 21:18:56 +0530

    Exception added

---
+++ Exceptions/custom_exception.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+struct MyException : public exception {
+   const char * what () const throw () {
+      return "C++ Exception";
+   }
+};
+ 
+int main() {
+   try {
+      throw MyException();
+   } catch(MyException& e) {
+      std::cout << "MyException caught" << std::endl;
+      std::cout << e.what() << std::endl;
+   } catch(std::exception& e) {
+      //Other errors
+   }
+}
+
+/**
+* Here, what() is a public method provided by exception class and 
+* it has been overridden by all the child exception classes. 
+* This returns the cause of an exception.
+**/
